368e0a46b2a24927bcafad10e0b5ce22f73b921c
Joseph Jegede <ohiwemeh@gmail.com>
2025-06-16 14:44:45 +0100

    pagination removed

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -21,12 +21,12 @@ useEffect(() => {
 
   return (
     <div className=''>
-      <div className='flex justify-center gap-6 my-10 '>
+      {/* <div className='flex justify-center gap-6 my-10 '>
             <button onClick={()=>setMenu('All')} className= { menu==="All"? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>All</button>
             <button onClick={()=>setMenu('Technology')} className= { menu==="Technology"? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Politics</button>
             <button onClick={()=>setMenu('Startup')} className= { menu==="Startup"? 'bg-black text-white py-1 px-4 rounded-sm' : ""}>Sport</button>
             <button onClick={()=>setMenu('Lifestyle')} className= { menu==="Lifestyle"? 'bg-black text-white py-1 px-4 rounded-sm' : ""} >LifeStyle</button>
-      </div>
+      </div> */}
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
         {blogs.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
             return <BlogItem key={index} id={item._id} image={item.image} title={item.title}  category={item.category}/>
