b4bb1abc2a4129a865e15f10f638099c95b3e892
Joseph Jegede <ohiwemeh@gmail.com>
2025-06-16 13:42:52 +0100

    thunder

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,35 +1,38 @@
 import BlogPage from './BlogPage';
 import { blog_data } from '@/Assets/assets';
 
-// export async function generateMetadata({ params }) {
-//      const { id } = await params
-//   // const blog = blog_data.find(item => item.id === id);
+export async function generateMetadata({ params }) {
+     const { id } = await params
+ const response = await fetch('http://localhost:3000/api/blog')
+
+const blogData = await response.json()
+  const blog = blogData.blogs.find(item => item._id === id);
 
   
 
-//   if (!blog) return {};
+  if (!blog) return {};
 
-//   console.log(blog);
+  console.log(blog);
   
 
-//   return {
-//     title: blog.title,
-//     description: blog.description?.slice(0, 150),
-//     openGraph: {
-//       title: blog.title,
-//       description: blog.description?.slice(0, 150),
-//       images: [blog.image],
-//       url: `https://african-time-five.vercel.app/blogs/${params.id}`,
-//       type: 'article',
-//     },
-//     twitter: {
-//       card: 'summary_large_image',
-//       title: blog.title,
-//       description: blog.description?.slice(0, 150),
-//       images: [blog.image],
-//     },
-//   };
-// }
+  return {
+    title: blog.title,
+    description: blog.description?.slice(0, 150),
+    openGraph: {
+      title: blog.title,
+      description: blog.description?.slice(0, 150),
+      images: blog.image,
+      url: `https://african-time-five.vercel.app/blogs/${params.id}`,
+      type: 'article',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: blog.title,
+      description: blog.description?.slice(0, 150),
+      images: blog.image,
+    },
+  };
+}
 
 export default async function Page({ params }) {
   try{
@@ -39,7 +42,7 @@ const { id: i } = await params;
 const blogData = await response.json()
   const blog = blogData.blogs.find(item => item._id === i);
 
-console.log(blog);
+
 
  if (!blog) return <div>Not Found</div>;
 
