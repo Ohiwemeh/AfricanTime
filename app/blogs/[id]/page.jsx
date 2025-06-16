6849104a18087d5ecc562b2e4fb154602a064fa7
Joseph Jegede <ohiwemeh@gmail.com>
2025-06-16 13:35:08 +0100

    thunder

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,34 +1,55 @@
 import BlogPage from './BlogPage';
 import { blog_data } from '@/Assets/assets';
 
-export async function generateMetadata({ params }) {
-  const blog = blog_data.find(item => item.id === params.id);
+// export async function generateMetadata({ params }) {
+//      const { id } = await params
+//   // const blog = blog_data.find(item => item.id === id);
 
-  if (!blog) return {};
+  
 
-  return {
-    title: blog.title,
-    description: blog.description?.slice(0, 150),
-    openGraph: {
-      title: blog.title,
-      description: blog.description?.slice(0, 150),
-      images: [blog.image],
-      url: `https://african-time-five.vercel.app/blogs/${params.id}`,
-      type: 'article',
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: blog.title,
-      description: blog.description?.slice(0, 150),
-      images: [blog.image],
-    },
-  };
-}
+//   if (!blog) return {};
 
-export default function Page({ params }) {
-  const blog = blog_data.find(item => item.id === params.id);
+//   console.log(blog);
+  
 
-  if (!blog) return <div>Not Found</div>;
+//   return {
+//     title: blog.title,
+//     description: blog.description?.slice(0, 150),
+//     openGraph: {
+//       title: blog.title,
+//       description: blog.description?.slice(0, 150),
+//       images: [blog.image],
+//       url: `https://african-time-five.vercel.app/blogs/${params.id}`,
+//       type: 'article',
+//     },
+//     twitter: {
+//       card: 'summary_large_image',
+//       title: blog.title,
+//       description: blog.description?.slice(0, 150),
+//       images: [blog.image],
+//     },
+//   };
+// }
+
+export default async function Page({ params }) {
+  try{
+const { id: i } = await params;
+ const response = await fetch('http://localhost:3000/api/blog')
+
+const blogData = await response.json()
+  const blog = blogData.blogs.find(item => item._id === i);
+
+console.log(blog);
+
+ if (!blog) return <div>Not Found</div>;
 
   return <BlogPage blog={blog} />;
+  }catch(error){
+    console.log(error)
+  }
+   
+ 
+
+
+ 
 }
